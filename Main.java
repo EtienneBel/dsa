a4a4be68a1dade24d25e2cf58ed6326baf6917a5
etienne <belemgnegreetienne@gmail.com>
2024-07-24 12:33:57 +0000

    init

---
+++ Main.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    public static List<Integer> reverseArray(List<Integer> a) {
+        System.out.println("input ");
+        System.out.println(a);
+
+        int size = a.size();
+        List<Integer> tmpArray = new ArrayList<>();
+
+        for (int i = size; i > 0; i--) {
+            tmpArray.add(a.get(i - 1));
+        }
+
+        return tmpArray;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> a = new ArrayList<>();
+        a.add(1);
+        a.add(4);
+        a.add(3);
+        a.add(2);
+        List<Integer> reverseData = reverseArray(a);
+        System.out.println("reverseData ");
+        System.out.println(reverseData);
+    }
+
+}
